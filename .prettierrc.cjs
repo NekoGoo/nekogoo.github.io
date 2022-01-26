93100c420f1d171550d74aed225cd372389a0fb2
NekoGoo <NekoGooBiz@gmail.com>
2022-01-25 23:44:54 -0600

    prettier preferences updated

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    // prettier.io/docs/en/configuration.html
+    // prettier.io/docs/en/options.html
+    // github.com/prettier/prettier-vscode#default-formatter
+    endOfLine: 'auto', // required for ESLint
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 160,
+    tabWidth: 2,
+}
