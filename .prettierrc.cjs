410d8a68ecf3af1e01241bb6e861ce591ea40b27
NekoGoo <NekoGooBiz@gmail.com>
2022-01-28 00:28:16 -0600

    minor format change

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,11 +1,13 @@
 module.exports = {
-    // prettier.io/docs/en/configuration.html
-    // prettier.io/docs/en/options.html
-    // github.com/prettier/prettier-vscode#default-formatter
-    endOfLine: 'auto', // required for ESLint
-    semi: true,
-    trailingComma: 'all',
-    singleQuote: true,
-    printWidth: 160,
-    tabWidth: 2,
-}
+  // https://prettier.io/docs/en/configuration.html
+  // https://prettier.io/docs/en/options.html
+  // https://github.com/prettier/prettier-vscode#default-formatter
+  // https://prettier.io/docs/en/configuration.html#sharing-configurations
+  endOfLine: 'auto', // required for ESLint
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 160,
+  tabWidth: 2,
+  bracketSameLine: true
+};
