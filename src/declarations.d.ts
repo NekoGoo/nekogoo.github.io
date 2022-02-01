13798925d8508f69e7d4629ccc4ca85019d56774
NekoGoo <NekoGooBiz@gmail.com>
2022-01-31 18:10:37 -0600

    enable .png assets

---
+++ src/declarations.d.ts
@@ -0,0 +1,5 @@
+// Wildcard module declarations: https://www.typescriptlang.org/docs/handbook/modules.html
+// https://stackoverflow.com/questions/52759220/importing-images-in-typescript-react-cannot-find-module
+
+declare module '*.png';
+// declare module '*.jpg';
