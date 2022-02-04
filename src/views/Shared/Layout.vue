512a40cf5949485e3bdc6c918c929b166db6cea9
NekoGoo <NekoGooBiz@gmail.com>
2022-02-04 05:55:21 -0600

    Vuex demo component added

--- src/views/Shared/Layout.vue
+++ src/views/Shared/Layout.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+// import HelloWorldVuex from '../../components/HelloWorldVuex.vue';
 import NavFooter from './NavFooter.vue';
 import NavHeader from './NavHeader.vue';
 </script>
 
 <template>
   <NavHeader />
+
+  <!-- <HelloWorldVuex msg="Hello Vue 3 + TypeScript + Vite" /> -->
 
   <router-view />
 
