09dc49f9ac29654e915dc49c71925d8eb1306ffa
NekoGoo <NekoGooBiz@gmail.com>
2022-07-30 02:34:27 -0500

    fix to make CoC styles global to feature module

--- angular/nekogoo-app/src/app/modules/coc/components/coc-layout/coc-layout.component.ts
+++ angular/nekogoo-app/src/app/modules/coc/components/coc-layout/coc-layout.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'coc-layout',
   templateUrl: './coc-layout.component.html',
   styleUrls: ['./coc-layout.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CocLayoutComponent implements OnInit {
   constructor() {}
