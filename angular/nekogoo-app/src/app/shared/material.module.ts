0b41839d7993bd17e6fbce3f808ba2fb6b39b0f6
NekoGoo <NekoGooBiz@gmail.com>
2022-10-17 04:34:54 -0500

    mat table module added

--- angular/nekogoo-app/src/app/shared/material.module.ts
+++ angular/nekogoo-app/src/app/shared/material.module.ts
@@ -10,6 +10,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -31,6 +32,7 @@ import { MatTreeModule } from '@angular/material/tree';
     MatChipsModule,
     MatTooltipModule,
     MatTreeModule,
+    MatTableModule,
   ],
   exports: [
     MatButtonModule,
@@ -47,6 +49,7 @@ import { MatTreeModule } from '@angular/material/tree';
     MatChipsModule,
     MatTooltipModule,
     MatTreeModule,
+    MatTableModule,
   ],
 })
 export class MaterialModule {}
