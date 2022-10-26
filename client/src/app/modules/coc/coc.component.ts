7313982f8c73c0dd23ff7a09645fb34e646d0a70
NekoGoo <NekoGooBiz@gmail.com>
2022-10-26 04:34:21 -0500

    swapped model seed for interface

--- client/src/app/modules/coc/coc.component.ts
+++ client/src/app/modules/coc/coc.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-// import { SaveSlot } from '@modules/erpge/data/save-slot';
-import { ItemSeed } from '@modules/erpge/data/seeds/item-seed';
-import { AttributeSeed } from '@modules/erpge/data/seeds/system-parts/attribute-seed';
-import { EffectSeed } from '@modules/erpge/data/seeds/system-parts/effect-seed';
-import { TagSeed } from '@modules/erpge/data/seeds/system-parts/tag-seed';
-import { Attribute } from '@modules/erpge/models/engine-parts/attribute';
-import { Effect } from '@modules/erpge/models/engine-parts/effect';
-import { Tag } from '@modules/erpge/models/engine-parts/tag';
-import { Item } from '@modules/erpge/models/item';
+import {
+  Attribute,
+  AttributeSeed,
+  Effect,
+  EffectSeed,
+  Tag,
+  TagSeed,
+} from './interfaces';
 
 @Component({
   selector: 'app-coc',
@@ -19,7 +18,7 @@ export class CocComponent implements OnInit {
   attributes: Attribute[];
   effects: Effect[];
   tags: Tag[];
-  items: Item[];
+  // items: Item[];
   // liveGame: SaveSlot;
 
   constructor() {
@@ -29,17 +28,18 @@ export class CocComponent implements OnInit {
     // console.log(`store items: ${store.state.items.data.length}`);
     // console.log(`store liveGame: ${store.state.liveGame}`);
 
-    this.attributes = new AttributeSeed().data;
-    this.effects = new EffectSeed().data;
-    this.tags = new TagSeed().data;
-    this.items = new ItemSeed().data;
+    this.attributes = AttributeSeed;
+    this.effects = EffectSeed;
+    this.tags = TagSeed;
+    // this.items = ItemSeed;
+
     // this.liveGame = new SaveSlot('Test Player');
 
     console.log(`store attributes: ${this.attributes.length}`);
     // console.log('store attributes: ', this.attributes);
     console.log(`store effects: ${this.effects.length}`);
     console.log(`store tags: ${this.tags.length}`);
-    console.log(`store items: ${this.items.length}`);
+    // console.log(`store items: ${this.items.length}`);
     // console.log(`store liveGame: ${this.liveGame}`);
   }
 
