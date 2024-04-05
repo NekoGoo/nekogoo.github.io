988b20a4989e293ffdb6f608aae74e81e9f09d62
NekoGoo <NekoGooBiz@gmail.com>
2024-04-04 23:07:00 -0500

    rename coc-nav-layout to css

--- client/src/app/modules/coc/components/coc-nav-layout/coc-nav-layout.component.ts
+++ client/src/app/modules/coc/components/coc-nav-layout/coc-nav-layout.component.ts
@@ -3,12 +3,12 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'coc-nav-layout',
   templateUrl: './coc-nav-layout.component.html',
-  styleUrls: ['./coc-nav-layout.component.scss'],
+  styleUrls: ['./coc-nav-layout.component.css'],
   // TODO: remove below line when styles no longer need to be global to work
   encapsulation: ViewEncapsulation.None,
 })
 export class CocNavLayoutComponent implements OnInit {
-  constructor() {}
+  // constructor() {}
 
   ngOnInit(): void {}
 }
