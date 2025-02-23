0d4575453c1f54e4839b2c4ce2643e5e147d405f
NekoGoo <NekoGooBiz@gmail.com>
2025-02-23 19:16:17 +0000

    Standalone Migration: Bootstrap the application using standalone APIs

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,13 +1,45 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { EntityDataModule } from '@ngrx/data';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import { AppModule } from './app/app.module';
+import { SharedModule } from '@shared/shared.module';
+import { counterReducer } from '@shared/store/counter/counter.reducer';
+import { entityConfig } from '@shared/store/entity-metadata';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      // --- Core imports ---
+      BrowserModule,
+      // StoreModule.forRoot({}, {}),
+      // StoreModule.forRoot(reducers, {
+      //   metaReducers,
+      // }),
+      StoreModule.forRoot({ counter: counterReducer }),
+      // Instrumentation must be imported after StoreModule (config is optional)
+      StoreDevtoolsModule.instrument({
+        maxAge: 25, // Retains last 25 states
+        logOnly: environment.production, // Restrict extension to log-only mode
+        connectInZone: true,
+      }),
+      EffectsModule.forRoot([]),
+      EntityDataModule.forRoot(entityConfig),
+      // --- Routing imports ---
+      AppRoutingModule,
+      // --- Shared imports ---
+      SharedModule,
+    ),
+    provideAnimations(),
+  ],
+}).catch((err) => console.error(err));
